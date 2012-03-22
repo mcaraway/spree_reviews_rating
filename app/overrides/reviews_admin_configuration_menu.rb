83c238387714fd35c10444350b9faad87a6fa9fa
Damiano <damiano.giacomello@diginess.it>
2012-03-22 09:49:35 +0100

    fixed all translation

--- app/overrides/reviews_admin_configuration_menu.rb
+++ app/overrides/reviews_admin_configuration_menu.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_review_settings_link_configuration_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => %q{<%= configurations_sidebar_menu_item t(:review_settings), admin_review_settings_path %>},
+                     :text => %q{<%= configurations_sidebar_menu_item t("reviews_rating.review_settings"), admin_review_settings_path %>},
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
