0c4b830a4cac149556bf793e814c509e9ed4f5ce
Damiano <damiano.giacomello@diginess.it>
2012-03-30 16:19:52 +0200

    fixed nil for txt_stars and improved rating counter - not counting null ratings.

--- app/helpers/spree/reviews_helper.rb
+++ app/helpers/spree/reviews_helper.rb
@@ -5,6 +5,7 @@ module Spree
     end
 
     def txt_stars(n, show_out_of = true)
+      return t('no_rated') unless n
       res = I18n.t('star', :count => n)
       res += ' ' + t('out_of_5') if show_out_of
       res
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,16 +2,13 @@ Spree::Product.class_eval do
   has_many :reviews
 
   def get_stars
-    {:star => avg_rating.round, :rcount => reviews_count}
+    {:star => ((2.0 * self.avg_rating).round / 2.0), :rcount => reviews_count}
   end
 
   def recalculate_rating
-    reviews_count = reviews.reload.approved.count
-    if reviews_count > 0
-      self.update_attributes(:avg_rating => self.reviews.approved.sum(:rating).to_f / reviews_count, :reviews_count => reviews_count)
-    else
-      self.update_attribute :avg_rating, 0
-    end
+    reviews_count = reviews.reload.approved.where('rating IS NOT NULL').count
+    avg_rating = (reviews_count > 0) ? (self.reviews.approved.sum(:rating).to_f / reviews_count) : 0
+    self.update_attributes(:avg_rating => avg_rating, :reviews_count => reviews_count)
   end
   
 end
