87ed51d8186600d3f7ce9e4b8efc64733da0a36e
Damiano <damiano.giacomello@diginess.it>
2012-04-15 12:02:15 +0200

    fixed migration

--- db/migrate/20081020220724_create_reviews.rb
+++ db/migrate/20081020220724_create_reviews.rb
@@ -1,6 +1,6 @@
 class CreateReviews < ActiveRecord::Migration
   def change
-    create_table :spree_reviews do |t|
+    create_table :reviews do |t|
       t.string  :name, :location
       t.decimal :rating
       t.text :review
