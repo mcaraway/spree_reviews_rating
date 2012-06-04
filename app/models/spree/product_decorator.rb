e43345e44170ce8422b4b187fd3e14570e296850
Riccardo Degan <riccardo.degan@diginess.it>
2012-06-04 18:08:10 +0200

    Add spree 1.1 compatibility

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -7,7 +7,7 @@ Spree::Product.class_eval do
 
   def recalculate_rating
     reviews_count = reviews.reload.approved.where('rating IS NOT NULL').count
-    avg_rating = (reviews_count > 0) ? (self.reviews.approved.sum(:rating).to_f / reviews_count) : 0
+    self.avg_rating = (reviews_count > 0) ? (self.reviews.approved.sum(:rating).to_f / reviews_count) : 0
     self.update_attributes(:avg_rating => avg_rating, :reviews_count => reviews_count)
   end
   
