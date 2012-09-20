38fc6dcaec9b01bb53177a2245037b8580b6364e
dominiksteiner <dominik.j.steiner@gmail.com>
2012-09-20 11:32:49 -0600

    change roles methods to namespaced spree_roles

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,6 +2,6 @@ Spree.user_class.class_eval do
   before_create :set_role
   
   def set_role
-    self.role_ids = Spree::Role.find_by_name("user").id
+    self.spree_role_ids = Spree::Role.find_or_create_by_name("user").id
   end    
 end
