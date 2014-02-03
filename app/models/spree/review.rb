295ae4ca7c84358afdb9c89e51da575580776dbf
unknown <Michael@.(none)>
2014-02-03 18:20:45 -0500

    Fixed defaul_Scope warnings

--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -8,7 +8,7 @@ module Spree
     validates_presence_of :name, :review
     validates_numericality_of :rating, :if => Proc.new {|r| !r.rating.blank?}
     
-    default_scope order("created_at DESC")
+    default_scope {order("created_at DESC")}
     scope :not_approved, where("approved = ?", false)
     scope :approved, lambda {|*args| {:conditions => ["(? = ?) or (approved = ?)", Spree::Config[:include_unapproved_reviews], true, true ]}}
     scope :oldest_first, :order => "created_at asc"
